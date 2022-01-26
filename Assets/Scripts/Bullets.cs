33a4bf90cdca549cc33f1e1d31ebc955fd84c5f9
Richard Lussier <lussi036@morris.umn.edu>
2022-01-26 13:17:54 -0600

    Move files to proper path
    Moved the files for the first unity lab up one level so that the files are visible. The .gitignore file was updated to include files for unity.
    
    Co-authored-by: Natasha-Zebrev <zebre001@morris.umn.edu>

---
+++ Assets/Scripts/Bullets.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+
+    [SerializeField] private Transform mainTransform;
+    [SerializeField] private float moveTime;
+    [SerializeField] private float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FireRoutine());
+
+        IEnumerator FireRoutine()
+        {
+            float elapsedTime = 0;
+            while(elapsedTime <= moveTime)
+            {
+                elapsedTime += Time.deltaTime;
+                mainTransform.position += mainTransform.up * moveSpeed * Time.deltaTime;
+                yield return null;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
